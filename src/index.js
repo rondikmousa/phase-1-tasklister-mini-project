1701d3d844df6f526a642c77504a4029f6b601af
rondikmousa <rondikmousaa@gmail.com>
2022-10-19 20:11:41 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+ const input = document.querySelector('#new-task-description')
+ document.querySelector('#create-task-form').addEventListener('submit', (e) => {
+e.preventDefault()
+  const newTask = document.createElement('li') 
+newTask.innerHTML = input.value
+document.querySelector('#tasks').appendChild(newTask)
+ })
   // your code here
 });
